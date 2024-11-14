473f802068d8b6f644484f404090b8f27632b1f5
BennyCasti <bencastillo0521@gmail.com>
2024-11-14 11:01:58 -0600

    Update operatingSystem.cpp

--- operatingSystem.cpp
+++ operatingSystem.cpp
@@ -23,8 +23,74 @@ int arrivalTime;
 int currentBurstIndex;
 int remainingTime;
 string state;
+
 Process(int id, vector<int> bursts, int arrival) : id(id), burstSequence(bursts), arrivalTime(arrival), currentBurstIndex(0), remainingTime(bursts[0]), state("ready") {}
 };
+
+class DustemOS {
+public:
+DustemOS() : loggedIn(false), processCounter(0) {
+users.push_back({"admin" , "password"});
+srand(time(0));
+}
+
+void runOS() {
+boot();
+while (!loggedIn) {
+loggedIn = login();
+}
+simulate();
+}
+
+void simulate() {
+string schedulingAlgorithm;
+cout << "Enter scheduling algorithm (FCFS or SJF): ";
+cin >> schedulingAlgorithm;
+
+int cycle = 0;
+int cycleLimit = rand() % MAX_CYCLE_LIMIT + 1;
+cout << "Simulation will run for a maximum of " << cycleLimit << " cycles.\n";
+
+queue<Process*> readyQueue;
+vector<Process*> waitingQueue;
+Process* currentProcess = nullptr;
+
+while (cycle < cycleLimit) {
+
+if (cycle % 5 == 0) {
+Process* newProcess = createRandomProcess();
+readyQueue.push(newProcess);
+cout << "Cycle " << cycle << ": Process " << newProcess->id << "starts running.\n";
+}
+
+if (currentProcess == nullptr && !readyQueue.empty()) {
+currentProcess = (schedulingAlgorithm == "FCFS") ? selectFCFS(readyQueue) : selectSJF(readyQueue);
+currentProcess->state = "running";
+cout << "Cycle " << cycle << ": Process " currentProcess->id << " starts running. \n";
+}
+
+if (currentProcess != nullptr) {
+  currentProcess->remainingTime--;
+if (currentProcess->remainingTime == 0) {
+    cout << "Cycle " << cycle << ": Process " currentProcess ->id << " completes CPU burst. \n";
+    handleBurstCompletion(currentProcess, readyQueue, waitingQueue);
+    currentProcess = nullptr;
+  }
+}
+
+updateWaitingQueue(waitingQueue, readyQueue);
+
+if (readyQueue.empty() && waitingQueue.empty() && currentProcess == nullptr) {
+cout << "Cycle " << cycle << ": Simulation complete. \n";
+break;
+}
+    cycle++;
+}
+
+if (cycle >=cycleLimit) {
+cout << "Simulation reached the random cycle limit of " << cycleLimit << " cycles. \n";
+}
+}
 
 private:
   vector <user> users;
@@ -147,17 +213,6 @@ cout << "Process " << p->id << " has finished all bursts. " << endl;
 }
 }
 };
-
-
-
-
-
-
-
-
-
-
-
 
 int main()
 {
